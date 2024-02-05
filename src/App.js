6e6442d12d51ebe5f17290024546f8ad45400f4f
sourav <souravchongrey@gmail.com>
2024-02-05 10:47:01 +0530

    simple templet web page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./page/Home";
+import About from "./page/About";
+import Contact from "./page/Contact";
+import NavBar from "./components/NavBar";
+
+import Charge from "./page/Charge";
+import Store from "./page/Store";
+import Product from "./page/Product";
+import Story from "./page/Story";
+import NotFound from "./page/NotFound";
+import Footer from "./components/Footer";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/charge" element={<Charge />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/story" element={<Story />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
